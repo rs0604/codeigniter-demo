d6bafd86f33d0e76a01a81c593a9d3c4eac8b0d7
shima@luxor-system.jp <shima@luxor-system.jp>
2019-05-18 23:24:29 +0900

    POST値がサーバに渡っていない不具合を修正

--- public_html/application/config/config.php
+++ public_html/application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://192.168.33.55';
+$config['base_url'] = 'https://192.168.33.55';
 
 /*
 |--------------------------------------------------------------------------

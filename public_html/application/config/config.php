a48b5254531fb6b85db9e2f22a9294e897808745
shima@luxor-system.jp <shima@luxor-system.jp>
2019-05-18 23:23:39 +0900

    index.php がURIに入らないよう修正

--- public_html/application/config/config.php
+++ public_html/application/config/config.php
@@ -35,7 +35,7 @@ $config['base_url'] = 'http://192.168.33.55';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'index.php';
+$config['index_page'] = '';
 
 /*
 |--------------------------------------------------------------------------
